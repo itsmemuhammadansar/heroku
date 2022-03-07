33a90a3c40d8f9549fdc7b3ef9ad508c601e9c0c
itsmemuhammadansar <97939717+itsmemuhammadansar@users.noreply.github.com>
2022-03-07 23:16:34 +0500

    .env file add

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ if (process.env.NODE_ENV !== 'production') {
 */
   const stripeSecretKey = process.env.STRIPE_SECRET_KEY
   const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
-  
+  const dotenv = require('dotenv');
   const express = require('express')
   const app = express()
   const fs = require('fs')
   const stripe = require('stripe')(stripeSecretKey)
   
+  dotenv.config({path:'.env'})
   app.set('view engine', 'ejs')
   app.use(express.json())
   app.use(express.static('public'))
